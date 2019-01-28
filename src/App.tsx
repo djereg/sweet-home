0c3f3b116066e3b14c44851e4adadb903d7a665c
djereg <djereg@gmail.com>
2019-01-28 15:04:44 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.scss';
+import Particles from 'react-particles-js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    private params = {};
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        There's no<br/>
+                        place like<br/>
+                        127.0.0.1
+                    </p>
+
+                </header>
+                <Particles
+                    className="App-particles"
+                    params={{
+                        particles: {
+                            number: {
+                                value: 80,
+                                density: {
+                                    enable: true,
+                                    value_area: 800
+                                }
+                            },
+                            size: {
+                                value: 3,
+                                random: true,
+                            },
+                            color: {
+                                value: '#ffffff'
+                            },
+                            opacity: {
+                                value: 0.4,
+                            },
+                            move: {
+                                enable: true,
+                                speed: 5,
+                            },
+                            line_linked: {
+                                enable: true,
+                                opacity: 0.3,
+                            }
+                        },
+                        interactivity:{
+                            events: {
+                                onclick: {
+                                    enable: true,
+                                    mode: 'push',
+                                }
+                            }
+                        },
+                        retina_detect: true
+                    }}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
